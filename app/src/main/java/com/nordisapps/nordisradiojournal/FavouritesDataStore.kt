d10578108077600710a445cbad13f2cd93c553cb
Нордис <rostikyacenko2@gmail.com>
2025-10-10 23:50:14 +0300

    Добавлен DataStore для избранного

---
+++ app/src/main/java/com/nordisapps/nordisradiojournal/FavouritesDataStore.kt
@@ -0,0 +1,11 @@
+package com.nordisapps.nordisradiojournal
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.stringSetPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "favorites")
+
+val FAVORITE_STATIONS_KEY = stringSetPreferencesKey("favorite_stations")
