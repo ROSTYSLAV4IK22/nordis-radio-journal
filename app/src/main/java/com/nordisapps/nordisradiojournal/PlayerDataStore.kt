288974be0542f5488a8867ce118196b4bacf12bc
Нордис <rostikyacenko2@gmail.com>
2025-10-24 22:32:59 +0300

    Добавлен ключ для DataStore

---
+++ app/src/main/java/com/nordisapps/nordisradiojournal/PlayerDataStore.kt
@@ -0,0 +1,5 @@
+package com.nordisapps.nordisradiojournal
+
+import androidx.datastore.preferences.core.stringPreferencesKey
+
+val RECENTLY_PLAYED_KEY = stringPreferencesKey("recently_played_stations")
