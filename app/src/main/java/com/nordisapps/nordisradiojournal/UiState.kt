0b037cfb5de42e292e33825aec34ac2fd4885669
Нордис <rostikyacenko2@gmail.com>
2025-10-24 22:32:13 +0300

    Добавлено поле bitrate в UiState

--- app/src/main/java/com/nordisapps/nordisradiojournal/UiState.kt
+++ app/src/main/java/com/nordisapps/nordisradiojournal/UiState.kt
@@ -5,6 +5,7 @@ data class UiState(
     val currentStation: Station? = null,
     val isPlaying: Boolean = false,
     val currentTrackTitle: String? = null,
+    val currentBitrate: Int? = null,
     val recentlyPlayedStations: List<Station> = emptyList(),
     val favouriteStations: List<Station> = emptyList()
 )
