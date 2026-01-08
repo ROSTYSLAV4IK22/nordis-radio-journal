13133ee6d354eb1ea5494a5f10fd8d214e494a12
Нордис <rostikyacenko2@gmail.com>
2026-01-08 04:18:17 +0200

    Add dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     id("com.google.gms.google-services") version "4.4.4" apply false
+    id("com.github.ben-manes.versions") version "0.53.0"
 }
