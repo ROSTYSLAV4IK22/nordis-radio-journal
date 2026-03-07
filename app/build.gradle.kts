6626e3711d755ff945aa73cc650b9d0bd36f99d2
Нордис <rostikyacenko2@gmail.com>
2026-03-07 17:07:16 +0200

    Bump to version 0.2.0-alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.nordisapps.nordisradiojournal"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0-alpha"
+        versionCode = 2
+        versionName = "0.2.0-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
